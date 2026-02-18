e1b87463048950fb2b615ec95e9dba4a9aa4a6c4
Dimitrios Lahanas <dimitrilahanas@gmail.com>
2026-02-18 22:40:45 +1100

    feat(App): completed solution for java prompt 01.

--- 01-Student_Marks_Summary/java/App.java
+++ 01-Student_Marks_Summary/java/App.java
@@ -1,5 +1,22 @@
 public class App {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        double total = 0;
+        int totalInputNums = 0;
+        double mark = In.askInt("Enter mark:");
+        while (mark != -1) {
+            total += mark;
+            totalInputNums++;
+            mark = In.askInt("Enter mark:");
+        }
+
+        if (totalInputNums == 0) {
+            System.out.println("No marks were entered.");
+        } else {
+            double averageMarks = total / totalInputNums;
+            System.out.println("Total mark value = " + total);
+            System.out.println("Total inputted marks = " + totalInputNums);
+            System.out.println("Average mark = " + averageMarks);
+        }
     }
 }
