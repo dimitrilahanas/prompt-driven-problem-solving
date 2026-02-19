a6f5728365c5e3586884fc9bd9d1a67ff158e892
Dimitrios Lahanas <dimitrilahanas@gmail.com>
2026-02-19 19:36:15 +1100

    add(App): added python solution.

--- 01-Student_Marks_Summary/python/App.py
+++ 01-Student_Marks_Summary/python/App.py
@@ -1,1 +1,16 @@
+totalInputNums = 0
+totalMarks = 0
+
 mark = int(input("Enter mark: "))
+while mark != -1:
+    totalMarks += mark
+    totalInputNums += 1
+    mark = int(input("Enter mark: "))
+
+if totalInputNums == 0:
+    print("No marks were entered.")
+else:
+    average = totalMarks / totalInputNums
+    print(f"Total mark value = {totalMarks}")
+    print(f"Total inputted marks = {totalInputNums}")
+    print(f"Average mark = {average}")
